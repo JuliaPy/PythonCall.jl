3ab5ff113e4d3ffe5f8e0254c124a4615104922f
Christopher Doris <github.com/cjdoris>
2020-11-20 09:56:13 +0000

    ensure libpython is opened with RTLD_GLOBAL

--- src/init.jl
+++ src/init.jl
@@ -3,6 +3,7 @@ const PYWPROGNAME = Base.cconvert(Cwstring, PYPROGNAME)
 const PYISSTACKLESS = false
 
 function __init__()
+    dlopen(PYLIB, RTLD_GLOBAL | RTLD_LAZY | RTLD_DEEPBIND)
     C.Py_SetPythonHome(pointer(PYWHOME))
     C.Py_SetProgramName(pointer(PYWPROGNAME))
     C.Py_Initialize()

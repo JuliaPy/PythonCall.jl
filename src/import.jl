fbf16e81fe3f254a97643973cd5785bd5d6d1d44
Christopher Doris <github.com/cjdoris>
2020-11-26 18:30:49 +0000

    more general pyimport syntax

--- src/import.jl
+++ src/import.jl
@@ -1,3 +1,18 @@
+"""
+    pyimport(m, ...)
+    pyimport(m => k, ...)
+
+Import and return the module `m`.
+
+If additionally `k` is given, then instead returns this attribute from `m`. If it is a tuple, a tuple of attributes is returned.
+
+If two or more arguments are given, they are all imported and returned as a tuple.
+"""
 pyimport(m::AbstractString) = check(C.PyImport_ImportModule(m))
 pyimport(m) = check(C.PyImport_Import(pyobject(m)))
+function pyimport(x::Pair)
+    m = pyimport(x[1])
+    x[2] isa Tuple ? map(k->pygetattr(m, k), x[2]) : pygetattr(m, x[2])
+end
+pyimport(m1, m2, ms...) = (pyimport(m1), pyimport(m2), map(pyimport, ms)...)
 export pyimport

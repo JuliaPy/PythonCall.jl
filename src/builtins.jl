26d83b8ee9f50f1a7a68d5c0f221f7ff6f746088
Christopher Doris <github.com/cjdoris>
2020-11-27 14:14:59 +0000

    load exception types directly from libpython

--- src/builtins.jl
+++ src/builtins.jl
@@ -21,25 +21,40 @@ for p in [:all, :any, :chr, :compile, :eval, :exec, :format, :help, :hex, :id, :
     @eval export $jf
 end
 
-# singletons, exceptions and warnings
-for p in [
-    :Ellipsis, :NotImplemented, :ArithmeticError, :AssertionError, :AttributeError,
-    :BaseException, :BlockingIOError, :BrokenPipeError, :BufferError, :BytesWarning,
-    :ChildProcessError, :ConnectionAbortedError, :ConnectionError, :ConnectionRefusedError,
-    :ConnectionResetError, :DeprecationWarning, :EOFError, :EnvironmentError, :Exception,
-    :FileExistsError, :FileNotFoundError, :FloatingPointError, :FutureWarning,
-    :GeneratorExit, :IOError, :ImportError, :ImportWarning, :IndentationError, :IndexError,
-    :InterruptedError, :IsADirectoryError, :KeyError, :KeyboardInterrupt, :LookupError,
-    :MemoryError, :ModuleNotFoundError, :NameError, :NotADirectoryError,
-    :NotImplementedError, :OSError, :OverflowError, :PendingDeprecationWarning,
-    :PermissionError, :ProcessLookupError, :RecursionError, :ReferenceError,
-    :ResourceWarning, :RuntimeError, :RuntimeWarning, :StopAsyncIteration, :StopIteration,
-    :SyntaxError, :SyntaxWarning, :SystemError, :SystemExit, :TabError, :TimeoutError,
-    :TypeError, :UnboundLocalError, :UnicodeDecodeError, :UnicodeEncodeError, :UnicodeError,
-    :UnicodeTranslateError, :UnicodeWarning, :UserWarning, :ValueError, :Warning,
-    :WindowsError, :ZeroDivisionError,
-]
+# singletons
+for p in [:Ellipsis, :NotImplemented]
     j = Symbol(:py, lowercase(string(p)))
     @eval const $j = PyLazyObject(() -> pybuiltins.$p)
+    @eval export $j
+end
+
+# exceptions and warnings
+# NOTE: We import these directly from C, so that the error indicator is not inadvertantly
+# reset when the lazy object is evaluated the first time (otherwise there can be problems
+# using `pyerroccurred(t)`).
+for n in [
+    # exceptions
+    :BaseException, :Exception, :ArithmeticError, :AssertionError, :AttributeError,
+    :BlockingIOError, :BrokenPipeError, :BufferError, :ChildProcessError,
+    :ConnectionAbortedError, :ConnectionError, :ConnectionRefusedError,
+    :ConnectionResetError, :EOFError, :FileExistsError, :FileNotFoundError,
+    :FloatingPointError, :GeneratorExit, :ImportError, :IndentationError, :IndexError,
+    :InterruptedError, :IsADirectoryError, :KeyError, :KeyboardInterrupt, :LookupError,
+    :MemoryError, :ModuleNotFoundError, :NameError, :NotADirectoryError,
+    :NotImplementedError, :OSError, :OverflowError, :PermissionError, :ProcessLookupError,
+    :RecursionError, :ReferenceError, :RuntimeError, :StopAsyncIteration, :StopIteration,
+    :SyntaxError, :SystemError, :SystemExit, :TabError, :TimeoutError, :TypeError,
+    :UnboundLocalError, :UnicodeDecodeError, :UnicodeEncodeError, :UnicodeError,
+    :UnicodeTranslateError, :ValueError, :ZeroDivisionError,
+    # aliases
+    :EnvironmentError, :IOError, :WindowsError,
+    # warnings
+    :Warning, :BytesWarning, :DeprecationWarning, :FutureWarning, :ImportWarning,
+    :PendingDeprecationWarning, :ResourceWarning, :RuntimeWarning, :SyntaxWarning,
+    :UnicodeWarning, :UserWarning,
+]
+    j = Symbol(:py, lowercase(string(n)))
+    p = QuoteNode(Symbol(:PyExc_, n))
+    @eval const $j = PyLazyObject(() -> pynewobject(unsafe_load(Ptr{CPyPtr}(C.@pyglobal($p))), true))
     @eval export $j
 end

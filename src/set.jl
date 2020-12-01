8169fa6c35eb4dec6de62e0a890338dd5d945ffc
Christopher Doris <github.com/cjdoris>
2020-12-01 15:24:15 +0000

    adds PySet

--- src/set.jl
+++ src/set.jl
@@ -8,5 +8,7 @@ pyisanyset(o::PyObject) = pyisset(o) || pyisfrozenset(o)
 export pyisset, pyisfrozenset, pyisanyset
 
 pyset(args...; opts...) = pysettype(args...; opts...)
+pyset(x::Union{Tuple,AbstractVector}) = pyset(pylist(x))
 pyfrozenset(args...; opts...) = pyfrozensettype(args...; opts...)
+pyfrozenset(x::Union{Tuple,AbstractVector}) = pyfrozenset(pylist(x))
 export pyset, pyfrozenset

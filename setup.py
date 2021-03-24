6477ac0f782eaf4df3c13e5e0535d500d1b921bb
Christopher Doris <github.com/cjdoris>
2021-03-24 12:45:06 +0000

    update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,16 @@
 import setuptools
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
     name="juliacall",
     version="0.1.0",
     author="Christopher Rowley",
-    # author_email="author@example.com",
     description="Julia-Python interoperability",
-    # long_description=long_description,
-    # long_description_content_type="text/markdown",
-    # url="https://github.com/pypa/sampleproject",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/cjdoris/PythonCall.jl",
     packages=["juliacall"],
     classifiers=[
         "Programming Language :: Python :: 3",

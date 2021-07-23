ee7080f6478cd323ecee6232f45e22e45c00d460
Christopher Doris <github.com/cjdoris>
2021-07-23 10:07:13 +0100

    rewrite Conda.activate

--- src/conda.jl
+++ src/conda.jl
@@ -13,23 +13,56 @@ create() = External.Conda.runconda(`create -y -p $(env())`)
 const ENV_STACK = Vector{Dict{String,String}}()
 
 function activate()
-    push!(ENV_STACK, copy(ENV))
     e = env()
-    shell = Sys.iswindows() ? "cmd.exe" : "posix"
-    pathsep = Sys.iswindows() ? ";" : ":"
-    # ask conda for a JSON description of how it would activate the environment
-    info = JSON.parse(read(External.Conda._set_conda_env(`$(External.Conda.conda) shell.$shell+json activate $e`), String))
-    # make these changes
-    # TODO: we currently ignore info["scripts"]["activate"]
-    #   run these in a subshell, print the resulting environment vars, and merge into ENV
-    #   (or just run the full `conda shell.* activate *` script)
-    ENV["PATH"] = join(info["path"]["PATH"], pathsep)
-    for k in info["vars"]["unset"]
-        delete!(ENV, k)
+    push!(ENV_STACK, copy(ENV))
+    try
+        # We run `conda shell.$shell activate $e` to get the shell script which activates
+        # the environment, then parse its instructions (taking advantage of the fact that
+        # conda outputs these instructions in quite a structured fashion) and make the
+        # necessary modifications.
+        #
+        # Another approach is to use `conda shell.$shell+json activate $e` to get the
+        # instructions as JSON, but as of July 2020 the latest version of conda on 32-bit
+        # linux does not support +json.
+        #
+        # Another approach is to actually execute the activate script in a subshell and then
+        # extract the resulting environment. This would capture environment variables set
+        # in scripts, which are currently ignored, and so replicate the conda environment
+        # precisely. Just need to work out how to get at the resulting environment.
+        if Sys.iswindows()
+            shell = "powershell"
+            exportvarregex = r"^\$Env:([^ =]+) *= *\"(.*)\"$"
+            setvarregex = exportvarregex
+            unsetvarregex = r"^Remove-Item \$Env:/([^ =]+)$"
+            runscriptregex = r"^\. +\"(.*)\"$"
+        else
+            shell = "posix"
+            exportvarregex = r"^export ([^ =]+)='(.*)'$"
+            setvarregex = r"^([^ =]+)='(.*)'"
+            unsetvarregex = r"^unset +([^ ]+)$"
+            runscriptregex = r"^\. +\"(.*)\"$"
+        end
+        for line in eachline(External.Conda._set_conda_env(`$(External.Conda.conda) shell.$shell activate $e`))
+            if (m = match(exportvarregex, line)) !== nothing
+                @debug "conda activate export var" k=m.captures[1] v=m.captures[2]
+                ENV[m.captures[1]] = m.captures[2]
+            elseif (m = match(setvarregex, line)) !== nothing
+                @debug "ignoring conda activate set var" k=m.captures[1] v=m.captures[2]
+            elseif (m = match(unsetvarregex, line)) !== nothing
+                @debug "conda activate unset var" k=m.captures[1]
+                delete!(ENV, m.captures[1])
+            elseif (m = match(runscriptregex, line)) !== nothing
+                @debug "ignoring conda activate script" file=m.captures[1]
+            elseif !isempty(strip(line))
+                @warn "ignoring conda activate line" line
+            end
+        end
+        @assert realpath(ENV["CONDA_PREFIX"]) == realpath(e)
+        return
+    catch
+        deactivate()
+        rethrow()
     end
-    ENV["CONDA_PREFIX"] = e
-    merge!(ENV, info["vars"]["export"])
-    return
 end
 
 function deactivate()

dcdee5c2c4c6ffb2889b3494037a86f981d21382
Christopher Doris <github.com/cjdoris>
2021-08-16 10:05:15 +0100

    slice should take at least one argument

--- src/concrete/slice.jl
+++ src/concrete/slice.jl
@@ -1,12 +1,12 @@
 # :PySlice_New => (PyPtr, PyPtr, PyPtr) => PyPtr,
 
 """
-    pyslice([[start], stop], [step])
+    pyslice([start], stop, [step])
 
 Construct a Python `slice`. Unspecified arguments default to `None`.
 """
 pyslice(x, y, z=pybuiltins.None) = pynew(errcheck(@autopy x y z C.PySlice_New(getptr(x_), getptr(y_), getptr(z_))))
-pyslice(y=pybuiltins.None) = pyslice(pybuiltins.None, y, pybuiltins.None)
+pyslice(y) = pyslice(pybuiltins.None, y, pybuiltins.None)
 export pyslice
 
 pyisslice(x) = pytypecheck(x, pybuiltins.slice)

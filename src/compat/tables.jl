88e861a2daaa77d9a5041b86734c74936015bf2c
Christopher Doris <github.com/cjdoris>
2021-09-24 09:04:53 +0100

    fix non-existent function

--- src/compat/tables.jl
+++ src/compat/tables.jl
@@ -74,7 +74,7 @@ function init_tables()
             codes = map(x -> x===missing ? -1 : Int(CategoricalArrays.levelcode(x))-1, x)
             cats = CategoricalArrays.levels(x)
             ordered = x.pool.ordered
-            pypandasmodule().Categorical.from_codes(codes, cats, ordered=ordered)
+            pyimport("pandas").Categorical.from_codes(codes, cats, ordered=ordered)
         end
     end
 end

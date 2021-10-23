26960e4655550cb40fbcf7f64c5c6946eaf3f091
Christopher Doris <github.com/cjdoris>
2021-10-23 15:56:19 +0100

    update python package metadata

--- setup.py
+++
@@ -1,6 +0,0 @@
-import site, sys, setuptools
-
-if __name__ == '__main__':
-    # https://github.com/pypa/pip/issues/7953#issuecomment-645133255
-    site.ENABLE_USER_SITE = "--user" in sys.argv[1:]
-    setuptools.setup()

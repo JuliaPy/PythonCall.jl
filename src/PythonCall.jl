bd775f937b7ca7b5c106f99526f201d32a17a8ec
Christopher Doris <github.com/cjdoris>
2021-11-11 18:10:38 +0000

    allow explicit conversion between Py and PyCall.PyObject

--- src/PythonCall.jl
+++ src/PythonCall.jl
@@ -107,6 +107,29 @@ function __init__()
         init_ipython()
         init_tables()
     end
+    @require PyCall="438e738f-606a-5dbb-bf0a-cddfbfd45ab0" init_pycall(PyCall)
+end
+
+function init_pycall(PyCall::Module)
+    # allow explicit conversion between PythonCall.Py and PyCall.PyObject
+    # provided they are using the same interpretr
+    errmsg = """
+    Conversion between `PyCall.PyObject` and `PythonCall.Py` is only possible when using the same Python interpreter.
+
+    There are two ways to achieve this:
+    - Set the environment variable `JULIA_PYTHONCALL_EXE` to `"@PyCall"`. This forces PythonCall to use the same
+      interpreter as PyCall, but PythonCall loses the ability to manage its own dependencies.
+    - Set the environment variable `PYTHON` to `PythonCall.C.CTX.exe_path` and rebuild PyCall. This forces PyCall
+      to use the same interpreter as PythonCall, but needs to be repeated whenever you switch Julia environment.
+    """
+    @eval function Py(x::$PyCall.PyObject)
+        C.CTX.matches_pycall::Bool || error($errmsg)
+        return pynew(C.PyPtr($PyCall.pyreturn(x)))
+    end
+    @eval function $PyCall.PyObject(x::Py)
+        C.CTX.matches_pycall::Bool || error($errmsg)
+        return $PyCall.PyObject($PyCall.PyPtr(incref(getptr(x))))
+    end
 end
 
 end

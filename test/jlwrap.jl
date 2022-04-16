6af57539e2358ee7c5ade5a2e022dc345ff6fd5e
Michael Boyle <michael.oliver.boyle@gmail.com>
2022-04-15 22:40:44 -0400

    Add numpy array-wrap tests

--- test/jlwrap.jl
+++ test/jlwrap.jl
@@ -5,3 +5,16 @@
     x4 = pyconvert(Vector{Int}, x3)
     @test x1 == x4
 end
+
+@testset "dtypes" begin
+    np = pyimport("numpy");
+    y = range(-5,5,length=11)
+    arr = np.asarray(y)
+    @test pyconvert(Int, arr.size) == 11
+    @test pyconvert(String, arr.dtype.name) == "float64"
+    @test all(iszero.(pyconvert(Any, arr) .- y))
+    arr32 = np.asarray(y, dtype=np.float32)
+    @test pyconvert(Int, arr32.size) == 11
+    @test pyconvert(String, arr32.dtype.name) == "float32"
+    @test all(iszero.(pyconvert(Any, arr32) .- y))
+end

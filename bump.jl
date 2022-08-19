5c5cab878f5b0fe0b5c362bc28974c3cfbe884a8
Christopher Doris <github.com/cjdoris>
2022-08-19 16:37:04 +0100

    add bump script [skip ci]

---
+++ bump.jl
@@ -0,0 +1,30 @@
+using Dates
+
+oldver = ARGS[1]
+newver = ARGS[2]
+
+function bump(file, oldpat, newpat)
+    println("Bumping $file...")
+    @assert oldpat != newpat
+    oldtext = read(file, String)
+    newtext = replace(oldtext, oldpat => newpat)
+    @assert newtext != oldtext
+    write(file, newtext)    
+end
+
+function bumpver(file, pattern, oldver, newver)
+    @assert oldver != newver
+    oldpat = replace(pattern, "{}" => oldver)
+    @assert oldpat != pattern
+    newpat = replace(pattern, "{}" => newver)
+    @assert newpat != pattern
+    bump(file, oldver, newver)
+end
+
+bumpver("Project.toml", "version = \"{}\"\n", oldver, newver)
+bumpver("setup.cfg", "version = {}\n", oldver, newver)
+bumpver("pysrc/juliacall/__init__.py", "__version__ = '{}'\n", oldver, newver)
+bumpver("pysrc/juliacall/juliapkg.json", "\"version\": \"={}\"", oldver, newver)
+bumpver("pysrc/juliacall/juliapkg-dev.json", "\"version\": \"={}\"", oldver, newver)
+bumpver("src/PythonCall.jl", "VERSION = v\"{}\"", oldver, newver)
+bump("docs/src/releasenotes.md", "## Unreleased", "## $newver ($(today()))")

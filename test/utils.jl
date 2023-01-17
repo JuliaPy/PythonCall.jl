df664dfba6e2621fa105880e090f55e27cc260d0
Carlo Lucibello <carlo.lucibello@gmail.com>
2023-01-17 09:39:00 +0100

    tests for Static String (#256)
    Adding tests from #254

--- test/utils.jl
+++ test/utils.jl
@@ -3,3 +3,11 @@
         @test PythonCall.Utils.mimes_for(x) isa Vector{String}
     end
 end
+
+@testitem "StaticString length and indexing" begin
+    s = PythonCall.Utils.StaticString{UInt32, 44}("ababababb")
+    @test length(s) == 9
+    @test s[1] == 'a'
+    @test s[1:2] == "ab"
+    @test s[1:2:end] == "aaaab"
+end

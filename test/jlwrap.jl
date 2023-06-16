101cf770880a3878f6a7c9240c292b395be06cd6
Christopher Doris <github.com/cjdoris>
2023-06-16 15:18:05 +0100

    add jlwrap tests

--- test/jlwrap.jl
+++ test/jlwrap.jl
@@ -17,6 +17,17 @@
     Base.:(>>)(x::Foo, y::Foo) = "$(x.value) >> $(y.value)"
     Base.:(&)(x::Foo, y::Foo) = "$(x.value) & $(y.value)"
     Base.:(|)(x::Foo, y::Foo) = "$(x.value) | $(y.value)"
+    @testset "type" begin
+        @test pyis(pytype(pyjl(Foo(1))), PythonCall.pyjlanytype)
+        @test pyis(pytype(pyjl(nothing)), PythonCall.pyjlanytype)
+        @test pyis(pytype(pyjl(missing)), PythonCall.pyjlanytype)
+    end
+    @testset "bool" begin
+        @test pytruth(pyjl(Foo(0)))
+        @test pytruth(pyjl(Foo(1)))
+        @test pytruth(pyjl(nothing))
+        @test pytruth(pyjl(missing))
+    end
     @testset "pos" begin
         z = pyjl(+Foo(1))
         @test pyconvert(String, z) == "+ 1"
@@ -123,10 +134,123 @@
     end
 end
 
+@testitem "array" begin
+    @testset "type" begin
+        @test pyis(pytype(pyjl(fill(nothing))), PythonCall.pyjlarraytype)
+        @test pyis(pytype(pyjl([1 2; 3 4])), PythonCall.pyjlarraytype)
+    end
+    @testset "bool" begin
+        @test !pytruth(pyjl(fill(nothing, 0, 1)))
+        @test !pytruth(pyjl(fill(nothing, 1, 0)))
+        @test pytruth(pyjl(fill(nothing)))
+        @test pytruth(pyjl(fill(nothing, 1, 2)))
+        @test pytruth(pyjl(fill(nothing, 1, 2, 3)))
+    end
+end
+
+@testitem "base" begin
+    
+end
+
+@testitem "callback" begin
+    
+end
+
+@testitem "dict" begin
+    @testset "type" begin
+        @test pyis(pytype(pyjl(Dict())), PythonCall.pyjldicttype)
+    end
+    @testset "bool" begin
+        @test !pytruth(pyjl(Dict()))
+        @test pytruth(pyjl(Dict("one"=>1, "two"=>2)))
+    end
+end
+
+@testitem "io" begin
+    @testset "type" begin
+        @test pyis(pytype(pyjl(devnull)), PythonCall.pyjlbinaryiotype)
+        @test pyis(pytype(pybinaryio(devnull)), PythonCall.pyjlbinaryiotype)
+        @test pyis(pytype(pytextio(devnull)), PythonCall.pyjltextiotype)
+    end
+    @testset "bool" begin
+        @test pytruth(pybinaryio(devnull))
+        @test pytruth(pytextio(devnull))
+    end
+end
+
 @testitem "iter" begin
     x1 = [1,2,3,4,5]
     x2 = pyjl(x1)
     x3 = pylist(x2)
     x4 = pyconvert(Vector{Int}, x3)
     @test x1 == x4
+end
+
+@testitem "module" begin
+    @testset "type" begin
+        @test pyis(pytype(pyjl(PythonCall)), PythonCall.pyjlmoduletype)
+    end
+    @testset "bool" begin
+        @test pytruth(pyjl(PythonCall))
+    end
+end
+
+@testitem "number" begin
+    @testset "type" begin
+        @test pyis(pytype(pyjl(false)), PythonCall.pyjlintegertype)
+        @test pyis(pytype(pyjl(0)), PythonCall.pyjlintegertype)
+        @test pyis(pytype(pyjl(0//1)), PythonCall.pyjlrationaltype)
+        @test pyis(pytype(pyjl(0.0)), PythonCall.pyjlrealtype)
+        @test pyis(pytype(pyjl(Complex(0.0))), PythonCall.pyjlcomplextype)
+    end
+    @testset "bool" begin
+        @test !pytruth(pyjl(false))
+        @test !pytruth(pyjl(0))
+        @test !pytruth(pyjl(0//1))
+        @test !pytruth(pyjl(0.0))
+        @test !pytruth(pyjl(Complex(0.0)))
+        @test pytruth(pyjl(true))
+        @test pytruth(pyjl(3))
+        @test pytruth(pyjl(5//2))
+        @test pytruth(pyjl(2.3))
+        @test pytruth(pyjl(Complex(1.2, 3.4)))
+    end
+end
+
+@testitem "objectarray" begin
+    
+end
+
+@testitem "raw" begin
+    
+end
+
+@testitem "set" begin
+    @testset "type" begin
+        @test pyis(pytype(pyjl(Set())), PythonCall.pyjlsettype)
+    end
+    @testset "bool" begin
+        @test !pytruth(pyjl(Set()))
+        @test pytruth(pyjl(Set([1,2,3])))
+    end
+end
+
+@testitem "type" begin
+    @testset "type" begin
+        @test pyis(pytype(pyjl(Int)), PythonCall.pyjltypetype)
+    end
+    @testset "bool" begin
+        @test pytruth(pyjl(Int))
+    end
+end
+
+@testitem "vector" begin
+    @testset "type" begin
+        @test pyis(pytype(pyjl([1, 2, 3, 4])), PythonCall.pyjlvectortype)
+    end
+    @testset "bool" begin
+        @test !pytruth(pyjl([]))
+        @test pytruth(pyjl([1]))
+        @test pytruth(pyjl([1,2]))
+    end
 end

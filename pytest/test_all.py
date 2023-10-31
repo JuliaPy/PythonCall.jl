4018135167535fa9c4f2f2ae1ba02d0e7628ce5c
Christopher Doris <github.com/cjdoris>
2023-10-31 10:23:55 +0000

    add test for issue #394

--- pytest/test_all.py
+++ pytest/test_all.py
@@ -1,2 +1,19 @@
 def test_import():
     import juliacall
+
+def test_issue_394():
+    "https://github.com/JuliaPy/PythonCall.jl/issues/394"
+    from juliacall import Main as jl
+    x = 3
+    f = lambda x: x+1
+    y = 5
+    jl.x = f
+    assert jl.x is x
+    jl.f = f
+    assert jl.f is f
+    jl.y = y
+    assert jl.y is y
+    assert jl.x is x
+    assert jl.f is f
+    assert jl.y is y
+    assert jl.seval("f(x)") == 4

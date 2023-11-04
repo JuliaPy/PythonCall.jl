f7bca8bdc9aa8845a6a334a6cdf05acb40e72f45
Christopher Doris <github.com/cjdoris>
2023-11-04 20:01:01 +0000

    suppress project.toml formatting test

--- test/aqua.jl
+++ test/aqua.jl
@@ -1,6 +1,7 @@
 @testitem "Aqua" begin
     # The unbound_args test fails on methods with signature like foo(::Type{Tuple{Vararg{V}}}) where V
     # Seems like a bug.
+    # Project.toml formatting is not consistent between Julia versions.
     import Aqua
-    Aqua.test_all(PythonCall, unbound_args=false)
+    Aqua.test_all(PythonCall, unbound_args=false, project_toml_formatting=Base.VERSION >= v"1.9")
 end

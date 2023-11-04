55b8991f66042f7e743aa25cb8c876c683221cc1
Christopher Doris <github.com/cjdoris>
2023-11-04 20:13:26 +0000

    i don't understand why the formatting test is failing :/

--- test/aqua.jl
+++ test/aqua.jl
@@ -2,5 +2,5 @@
     # The unbound_args test fails on methods with signature like foo(::Type{Tuple{Vararg{V}}}) where V
     # Seems like a bug.
     import Aqua
-    Aqua.test_all(PythonCall, unbound_args=false)
+    Aqua.test_all(PythonCall, unbound_args=false, project_toml_formatting=false)
 end

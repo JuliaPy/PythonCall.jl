414fb9120e952cba6219a00183df1a33bfa8f9f9
Christopher Doris <github.com/cjdoris>
2023-11-04 17:38:08 +0000

    basic gui tests

--- test/compat.jl
+++ test/compat.jl
@@ -16,3 +16,13 @@
         @test !isdir(tname)
     end
 end
+
+@testitem "gui" begin
+    @test PythonCall.fix_qt_plugin_path() isa Bool
+    @test PythonCall.fix_qt_plugin_path() === false
+    for g in [:pyqt4, :pyqt5, :pyside, :pyside2, :gtk, :gtk3, :wx, :tkinter]
+        # TODO: actually test the various GUIs somehow?
+        @test_throws PyException PythonCall.event_loop_on(g)
+        @test PythonCall.event_loop_off(g) === nothing
+    end
+end

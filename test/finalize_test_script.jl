a5a2c96bfa41bbbe814f466f848f754ea67bfc5d
Christopher Doris <github.com/cjdoris>
2024-07-31 19:44:34 +0100

    restore test script

--- test/finalize_test_script.jl
+++ test/finalize_test_script.jl
@@ -7,17 +7,3 @@ let
         finalize(obj)
     end
 end
-
-@show isready(PythonCall.GC.QUEUE)
-GC.gc()
-# with GCHook, the queue should be empty now (the above gc() triggered GCHook to clear the PythonCall QUEUE)
-# without GCHook, gc() has no effect on the QUEUE
-@show isready(PythonCall.GC.QUEUE)
-GC.gc()
-@show isready(PythonCall.GC.QUEUE)
-GC.gc()
-@show isready(PythonCall.GC.QUEUE)
-# with GCHook this is not necessary, GC.gc() is enough
-# without GCHook, this is required to free any objects in the PythonCall QUEUE
-PythonCall.GC.gc()
-@show isready(PythonCall.GC.QUEUE)

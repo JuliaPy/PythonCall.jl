13cc34648e8098b227a2d1d3778aa134196ad594
Christopher Doris <github.com/cjdoris>
2024-08-01 22:22:36 +0100

    add gc tests

--- pytest/test_all.py
+++ pytest/test_all.py
@@ -96,6 +96,7 @@ def test_julia_gc():
                 finalize(obj)
             end
         end
+        Threads.nthreads() > 1 && @test !isempty(PythonCall.GC.QUEUE.items)
         GC.gc()
         @test isempty(PythonCall.GC.QUEUE.items)
         """
--- test/GC.jl
+++ test/GC.jl
@@ -1,1 +1,32 @@
-# TODO
+@testset "201: GC segfaults" begin
+    # https://github.com/JuliaPy/PythonCall.jl/issues/201
+    # This should not segfault!
+    cmd = Base.julia_cmd()
+    path = joinpath(@__DIR__, "finalize_test_script.jl")
+    p = run(`$cmd -t2 --project $path`)
+    @test p.exitcode == 0
+end
+
+@testset "GC.gc()" begin
+    let
+        pyobjs = map(pylist, 1:100)
+        Threads.@threads for obj in pyobjs
+            finalize(obj)
+        end
+    end
+    Threads.nthreads() > 1 && @test !isempty(PythonCall.GC.QUEUE.items)
+    PythonCall.GC.gc()
+    @test isempty(PythonCall.GC.QUEUE.items)
+end
+
+@testset "GC.GCHook" begin
+    let
+        pyobjs = map(pylist, 1:100)
+        Threads.@threads for obj in pyobjs
+            finalize(obj)
+        end
+    end
+    Threads.nthreads() > 1 && @test !isempty(PythonCall.GC.QUEUE.items)
+    GC.gc()
+    @test isempty(PythonCall.GC.QUEUE.items)
+end

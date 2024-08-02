73f7eb8fa1d96c98434d60ddf50f54ddcd031a7e
Christopher Doris <github.com/cjdoris>
2024-08-02 21:06:03 +0100

    delete redundant test

--- test/GC.jl
+++ test/GC.jl
@@ -1,12 +1,3 @@
-@testitem "201: GC segfaults" begin
-    # https://github.com/JuliaPy/PythonCall.jl/issues/201
-    # This should not segfault!
-    cmd = Base.julia_cmd()
-    path = joinpath(@__DIR__, "finalize_test_script.jl")
-    p = run(`$cmd -t2 --project $path`)
-    @test p.exitcode == 0
-end
-
 @testitem "GC.gc()" begin
     let
         pyobjs = map(pylist, 1:100)

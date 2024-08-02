37c89d7546f0ef6749190e9b1e9a8438497073bb
MilesCranmer <miles.cranmer@gmail.com>
2024-08-02 16:28:53 +0100

    benchmarks: remove unused code

--- benchmark/gcbench.jl
+++ benchmark/gcbench.jl
@@ -1,19 +1,5 @@
 using PythonCall, Test
 
-# https://github.com/JuliaCI/GCBenchmarks/blob/fc288c696381ebfdef8f002168addd0ec1b08e34/benches/serial/append/append.jl
-macro gctime(ex)
-    quote
-        local prior = PythonCall.GC.SECONDS_SPENT_IN_GC[]
-        local ret = @timed $(esc(ex))
-        Base.time_print(stdout, ret.time * 1e9, ret.gcstats.allocd, ret.gcstats.total_time, Base.gc_alloc_count(ret.gcstats); msg="Runtime")
-        local after = PythonCall.GC.SECONDS_SPENT_IN_GC[]
-        println(stdout)
-        Base.time_print(stdout, (after - prior) * 1e9; msg="Python GC time")
-        println(stdout)
-        ret.value
-    end
-end
-
 function append_lots(iters=100 * 1024, size=1596)
     v = pylist()
     for i = 1:iters

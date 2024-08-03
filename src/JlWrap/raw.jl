276e80f8c4b6389c90adf6a4351619b0ff289cfc
Christopher Doris <github.com/cjdoris>
2024-08-03 11:05:58 +0100

    add RawValue._jl_call_nogil

--- src/JlWrap/raw.jl
+++ src/JlWrap/raw.jl
@@ -40,6 +40,22 @@ function pyjlraw_call(self, args_::Py, kwargs_::Py)
     ans
 end
 
+function pyjlraw_call_nogil(self, args_::Py, kwargs_::Py)
+    if pylen(kwargs_) > 0
+        args = pyconvert(Vector{Any}, args_)
+        kwargs = pyconvert(Dict{Symbol,Any}, kwargs_)
+        ans = pyjlraw(GIL.@release self(args...; kwargs...))
+    elseif pylen(args_) > 0
+        args = pyconvert(Vector{Any}, args_)
+        ans = pyjlraw(GIL.@release self(args...))
+    else
+        ans = pyjlraw(GIL.@release self())
+    end
+    pydel!(args_)
+    pydel!(kwargs_)
+    ans
+end
+
 pyjlraw_len(self) = Py(length(self))
 
 function pyjlraw_getitem(self, k_::Py)
@@ -130,6 +146,8 @@ class RawValue(ValueBase):
         return self._jl_callmethod($(pyjl_methodnum(pyjlraw_bool)))
     def _jl_any(self):
         return self._jl_callmethod($(pyjl_methodnum(pyjl)))
+    def _jl_call_nogil(self, *args, **kwargs):
+        return self._jl_callmethod($(pyjl_methodnum(pyjlraw_call_nogil)), args, kwargs)
 """,
             @__FILE__(),
             "exec",

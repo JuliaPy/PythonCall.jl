4fa1228ac46b5b781b7583e43ca01280411b149e
Christopher Doris <github.com/cjdoris>
2024-08-15 17:01:39 +0100

    test the gc queue is not empty while gil still unlocked

--- test/GC.jl
+++ test/GC.jl
@@ -1,11 +1,13 @@
 @testitem "GC.gc()" begin
     let
         pyobjs = map(pylist, 1:100)
-        PythonCall.GIL.@unlock Threads.@threads for obj in pyobjs
-            finalize(obj)
+        PythonCall.GIL.@unlock begin
+            Threads.@threads for obj in pyobjs
+                finalize(obj)
+            end
+            Threads.nthreads() > 1 && @test !isempty(PythonCall.GC.QUEUE.items)
         end
     end
-    Threads.nthreads() > 1 && @test !isempty(PythonCall.GC.QUEUE.items)
     PythonCall.GC.gc()
     @test isempty(PythonCall.GC.QUEUE.items)
 end
@@ -13,11 +15,13 @@ end
 @testitem "GC.GCHook" begin
     let
         pyobjs = map(pylist, 1:100)
-        PythonCall.GIL.@unlock Threads.@threads for obj in pyobjs
-            finalize(obj)
+        PythonCall.GIL.@unlock begin
+            Threads.@threads for obj in pyobjs
+                finalize(obj)
+            end
+            Threads.nthreads() > 1 && @test !isempty(PythonCall.GC.QUEUE.items)
         end
     end
-    Threads.nthreads() > 1 && @test !isempty(PythonCall.GC.QUEUE.items)
     GC.gc()
     @test isempty(PythonCall.GC.QUEUE.items)
 end

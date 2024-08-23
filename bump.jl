0958a7f3f85db7255ff93efbce45b47ee6f7c644
MilesCranmer <miles.cranmer@gmail.com>
2024-08-23 11:49:25 +0800

    build: update `bump.jl` with pyproject.toml

--- bump.jl
+++ bump.jl
@@ -22,7 +22,7 @@ function bumpver(file, pattern, oldver, newver)
 end
 
 bumpver("Project.toml", "version = \"{}\"\n", oldver, newver)
-bumpver("setup.cfg", "version = {}\n", oldver, newver)
+bumpver("pyproject.toml", "version = \"{}\"\n", oldver, newver)
 bumpver("pysrc/juliacall/__init__.py", "__version__ = '{}'\n", oldver, newver)
 bumpver("pysrc/juliacall/juliapkg.json", "\"version\": \"={}\"", oldver, newver)
 bumpver("pysrc/juliacall/juliapkg-dev.json", "\"version\": \"={}\"", oldver, newver)

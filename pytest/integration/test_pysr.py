3391801c689ad82c69c60bd25b6f08620a5137b9
MilesCranmer <miles.cranmer@gmail.com>
2024-08-23 11:29:23 +0800

    test: ensure we can see the integration test output

--- pytest/integration/test_pysr.py
+++ pytest/integration/test_pysr.py
@@ -7,7 +7,8 @@ def test_integration_pysr():
     import tempfile
 
     with tempfile.TemporaryDirectory() as tempdir:
-        subprocess.run([sys.executable, "-m", "virtualenv", tempdir], check=True)
+        run_kws = dict(check=True, capture_output=True)
+        subprocess.run([sys.executable, "-m", "virtualenv", tempdir], **run_kws)
 
         virtualenv_path = os.path.join(
             tempdir, "Scripts" if platform.system() == "Windows" else "bin"
@@ -17,11 +18,11 @@ def test_integration_pysr():
         assert os.path.exists(virtualenv_executable)
 
         # Install this package
-        subprocess.run([virtualenv_executable, "-m", "pip", "install", "."], check=True)
+        subprocess.run([virtualenv_executable, "-m", "pip", "install", "."], **run_kws)
         # Install PySR with no requirement on JuliaCall
         subprocess.run(
             [virtualenv_executable, "-m", "pip", "install", "--no-deps", "pysr"],
-            check=True,
+            **run_kws,
         )
         # Install PySR test requirements
         subprocess.run(
@@ -37,9 +38,9 @@ def test_integration_pysr():
                 "setuptools",
                 "pytest",
             ],
-            check=True,
+            **run_kws,
         )
         # Run PySR main test suite
         subprocess.run(
-            [virtualenv_executable, "-m", "pysr", "test", "main"], check=True
+            [virtualenv_executable, "-m", "pysr", "test", "main"], **run_kws,
         )

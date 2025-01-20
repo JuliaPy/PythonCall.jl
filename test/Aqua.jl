0b8029fb92ef909e009135ce0b582bce59cb4c71
Christopher Doris <github.com/cjdoris>
2025-01-20 14:04:19 +0000

    remove unused REPL dependency

--- test/Aqua.jl
+++ test/Aqua.jl
@@ -2,9 +2,5 @@
     # The unbound_args test fails on methods with signature like foo(::Type{Tuple{Vararg{V}}}) where V
     # Seems like a bug.
     import Aqua
-    Aqua.test_all(
-        PythonCall,
-        unbound_args = false,
-        stale_deps = (; ignore = [:REPL])
-    )
+    Aqua.test_all(PythonCall, unbound_args = false)
 end

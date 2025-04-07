f40e78d4f15f2942cb2d2825b52240f08ee322e5
Christopher Doris <github.com/cjdoris>
2025-04-07 21:10:14 +0100

    @kwdef not exported in 1.6

--- src/PythonCall.jl
+++ src/PythonCall.jl
@@ -6,7 +6,7 @@ module Internals
 
 using ..PythonCall
 
-@kwdef mutable struct Config
+Base.@kwdef mutable struct Config
     meta::String = ""
     auto_sys_last_traceback::Bool = true
     auto_fix_qt_plugin_path::Bool = true

28ea9d119ac36cb93fb32e5c1793eb045c8fa4dd
Christopher Doris <github.com/cjdoris>
2025-06-06 12:55:43 +0100

    remove disused function

--- src/Utils/Utils.jl
+++ src/Utils/Utils.jl
@@ -22,24 +22,6 @@ function explode_union(T)
 end
 
 """
-    pointer_from_obj(x)
-
-Returns `(p, c)` where `Base.pointer_from_objref(p) === x`.
-
-The pointer remains valid provided the object `c` is not garbage collected.
-"""
-function pointer_from_obj(o::T) where {T}
-    if T.mutable
-        c = o
-        p = Base.pointer_from_objref(o)
-    else
-        c = Ref{Any}(o)
-        p = unsafe_load(Ptr{Ptr{Cvoid}}(Base.pointer_from_objref(c)))
-    end
-    p, c
-end
-
-"""
     ExtraNewline(x)
 
 An object that displays the same as `x` but with an extra newline in text/plain.

121b36e749e11d17db1965015af65d28b7d8f75d
Klamkin, Michael <mklamkin3@gatech.edu>
2025-07-22 19:14:30 -0400

    Add name guard to init

--- pysrc/juliacall/init.py
+++ pysrc/juliacall/init.py
@@ -1,10 +1,11 @@
-import juliacall as _  # calls init() which calls juliapkg.executable() which lazily downloads julia
+if __name__ == '__main__':
+    import juliacall as _  # calls init() which calls juliapkg.executable() which lazily downloads julia
 
-import sys
-if "--debug" in sys.argv:
-    import juliapkg, json
-    state = juliapkg.state.STATE
-    state["version"] = str(state["version"])
-    print(json.dumps(state, indent=2))
-else:
-    print("Initialized successfully. Pass --debug to see the full JuliaPkg state.")
+    import sys
+    if "--debug" in sys.argv:
+        import juliapkg, json
+        state = juliapkg.state.STATE
+        state["version"] = str(state["version"])
+        print(json.dumps(state, indent=2))
+    else:
+        print("Initialized successfully. Pass --debug to see the full JuliaPkg state.")

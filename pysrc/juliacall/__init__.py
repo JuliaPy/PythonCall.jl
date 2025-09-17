bff7fd99651687173f3df7ac7bcf16fdcf49fdf6
Christopher Doris <github.com/cjdoris>
2025-09-17 17:26:13 +0100

    allow exe option to be found in the path

--- pysrc/juliacall/__init__.py
+++ pysrc/juliacall/__init__.py
@@ -96,6 +96,16 @@ def init():
                 raise ValueError(f'{s}: path does not exist')
             return os.path.abspath(path), s
         return default, s
+    
+    def executable_option(name, default=None, **kw):
+        import shutil
+        _path, s = option(name, **kw)
+        if _path is not None:
+            path = shutil.which(_path)
+            if path is None:
+                raise ValueError(f'{s}: executable not found')
+            return os.path.abspath(path), s
+        return default, s
 
     def int_option(name, *, accept_auto=False, **kw):
         val, s = option(name, **kw)
@@ -148,7 +158,7 @@ def init():
     CONFIG['opt_startup_file'] = choice('startup_file', ['yes', 'no'])[0]
     CONFIG['opt_heap_size_hint'] = option('heap_size_hint')[0]
     CONFIG['project'] = path_option('project', check_exists=True)[0]
-    CONFIG['exepath'] = path_option('exe', check_exists=True)[0]
+    CONFIG['exepath'] = executable_option('exe')[0]
 
     # Stop if we already initialised
     if CONFIG['inited']:

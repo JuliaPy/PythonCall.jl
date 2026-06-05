81967f912b071820a6b3e639220c13f559345de4
Christopher Rowley <github.com/cjdoris>
2026-06-05 15:20:46 +0100

    disable most of pythoncall while developing

--- src/PythonCall.jl
+++ src/PythonCall.jl
@@ -6,26 +6,26 @@ include("API/API.jl")
 include("Utils/Utils.jl")
 include("NumpyDates/NumpyDates.jl")
 include("C/C.jl")
-include("GIL/GIL.jl")
-include("GC/GC.jl")
-include("Core/Core.jl")
-include("Convert/Convert.jl")
-include("PyMacro/PyMacro.jl")
-include("Wrap/Wrap.jl")
-include("JlWrap/JlWrap.jl")
-include("Compat/Compat.jl")
+# include("GIL/GIL.jl")
+# include("GC/GC.jl")
+# include("Core/Core.jl")
+# include("Convert/Convert.jl")
+# include("PyMacro/PyMacro.jl")
+# include("Wrap/Wrap.jl")
+# include("JlWrap/JlWrap.jl")
+# include("Compat/Compat.jl")
 
-# not API but used in tests
-for k in [
-    :pyjlanytype,
-    :pyjlarraytype,
-    :pyjlvectortype,
-    :pyjlbinaryiotype,
-    :pyjltextiotype,
-    :pyjldicttype,
-    :pyjlsettype,
-]
-    @eval using .JlWrap: $k
-end
+# # not API but used in tests
+# for k in [
+#     :pyjlanytype,
+#     :pyjlarraytype,
+#     :pyjlvectortype,
+#     :pyjlbinaryiotype,
+#     :pyjltextiotype,
+#     :pyjldicttype,
+#     :pyjlsettype,
+# ]
+#     @eval using .JlWrap: $k
+# end
 
 end

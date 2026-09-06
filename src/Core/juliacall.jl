6ace1d1cf88a3484aa0ddd3aaa2e534569b8edce
Christopher Rowley <github.com/cjdoris>
2026-09-06 13:29:04 +0100

    guard an incref

--- src/Core/juliacall.jl
+++ src/Core/juliacall.jl
@@ -31,5 +31,5 @@ function init_juliacall()
         @assert !pybool_asbool(jl.CONFIG["init"])
     end
     pycopy!(pyJuliaError, jl.JuliaError)
-    CPyExc_JuliaError[] = incref(getptr(pyJuliaError))
+    CPyExc_JuliaError[] = C.@withts incref(getptr(pyJuliaError))
 end

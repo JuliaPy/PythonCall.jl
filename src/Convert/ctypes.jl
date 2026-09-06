93a696c5416da8c11aed3be40c711a03ac0e4b12
Christopher Rowley <github.com/cjdoris>
2026-09-06 13:25:11 +0100

    re-enable Convert module

--- src/Convert/ctypes.jl
+++ src/Convert/ctypes.jl
@@ -1,7 +1,7 @@
 struct pyconvert_rule_ctypessimplevalue{R,S} <: Function end
 
 function (::pyconvert_rule_ctypessimplevalue{R,SAFE})(::Type{T}, x::Py) where {R,SAFE,T}
-    Base.GC.@preserve x begin
+    C.@withts Base.GC.@preserve x begin
         ptr = C.PySimpleObject_GetValue(Ptr{R}, x)
         ans = unsafe_load(ptr)
         if SAFE
